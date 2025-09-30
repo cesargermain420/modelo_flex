28bd6ae8b9c1c6c5a0c27cbd7eb7c90dbb8adf34
Cesar <cesar.germain@gmail.com>
2025-09-30 17:17:33 +0200

    alignItems y Justify

--- App.tsx
+++ App.tsx
@@ -5,33 +5,54 @@ export default function App() {
   return (
     <>
     <View style={styles.contenedor1}>
-      <Text>Contenedor 1</Text>
+      <Text style={styles.texto}>Contenedor 1.a</Text>
+      <Text style={styles.texto}>Contenedor 1.b</Text>
+      <Text style={styles.texto}>Contenedor 1.c</Text>
     </View>
+
     <View style={styles.contenedor2}>
-      <Text>Contenedor 2</Text>
+      <Text>Contenedor 2.a</Text>
+      <Text>Contenedor 2.b</Text>
+      <Text>Contenedor 2.c</Text>    
     </View>
+
     <View style={styles.contenedor3}>
-      <Text>Contenedor 3</Text>
+      <Text>Contenedor 3.a</Text>
+      <Text>Contenedor 3.b</Text>
+      <Text>Contenedor 3.c</Text>    
     </View>
     </>
-
   )
 }
 
 const styles = StyleSheet.create({
+  texto:{
+    backgroundColor:"yellow"
+
+  },
 
 contenedor1:{
   flex:1,//Hacemos que crezca usando todo el espacio del telefono
-  backgroundColor:"red"
+  backgroundColor:"red",
+  justifyContent:"space-around",//CONTROLA EL EJE PRINCIPAL (VERTICAL)
+  alignItems:"center" //CONTROLA EL EJE SECUNDARIO(HORIZONTAL)
 
 },
+
+
 contenedor2:{
 flex:1,
-backgroundColor:"orange"
+backgroundColor:"orange",
+justifyContent:"space-between",  //DECIDE LA SEPARACION
+flexDirection:"row" //LOS PONE EN HORIZONTAL
 },
+
+
 contenedor3:{
 flex:1,
-backgroundColor:"green"
+backgroundColor:"green",
+flexDirection:"column", //LOS PONE EN VERTICAL INVERTIDO
+justifyContent:"center"
 },
 
 })
